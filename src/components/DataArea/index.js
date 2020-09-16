280a82e295caa822c09100f23e976f41c84a9a73
sthapa <san20hal@gmail.com>
2020-09-15 20:59:03 -0400

    functionality added

--- src/components/DataArea/index.js
+++ src/components/DataArea/index.js
@@ -82,7 +82,7 @@ return b[heading]-  a[heading];
             filteredUsers: results.data.results
           });
         });
-      }, []);
+      }, [developerState]);
     
       return (
         <DataAreaContext.Provider
